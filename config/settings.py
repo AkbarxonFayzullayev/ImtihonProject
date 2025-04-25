2c8dd8c073b67ffe800539f31d734c0520326ab9
AkbarxonFayzullayev <akbarxonfayzullayev555@gmail.com>
2025-04-25 21:33:18 +0500

    dfdf

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -130,12 +131,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Statik fayllar joylashgan papkalarni ko'rsating
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'config/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
